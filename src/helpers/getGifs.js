4d54786a84ba1c62a4c6e08d4bb52f10a516235a
Constantino Alexopoulos Real <tak@tagtak.com>
2022-08-16 18:05:51 +0200

    https añadidoo

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async (category) => {
-  const url = `http://api.giphy.com/v1/gifs/search?api_key=qLMpoHlAN0srdfmBQFpsz9GYo34mgTKw&limit=6&q=${category}`;
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=qLMpoHlAN0srdfmBQFpsz9GYo34mgTKw&limit=6&q=${category}`;
   const resp = await fetch(url);
   const { data } = await resp.json();
 
